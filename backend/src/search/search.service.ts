fa7960fb4b6afbd23c13bd7a37280c37d562cb3b
tmatis <tmatis@student.42.fr>
2023-09-11 11:31:30 +0200

    ✨ better parsing of youtube URL

--- backend/src/search/search.service.ts
+++ backend/src/search/search.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import ytsr from 'ytsr';
-import ytdl from 'ytdl-core';
 import ytpl from 'ytpl';
 import { Search } from './model/search.model';
 import { Video } from './model/video.model';
@@ -8,107 +7,71 @@ import { Channel } from './model/channel.model';
 import { Playlist } from './model/playlist.model';
 import { Mix } from './model/mix.model';
 import getFormattedTime from 'src/utils/get-formated-time';
+import { video_info } from 'play-dl';
+import throwExpression from 'src/utils/throw-expression';
 
-// https://youtu.be/WP2XAGZPh3w?list=RDWP2XAGZPh3w should be matched
-const MATCH_VIDEO_LINK =
-  /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})(\?[\w=&]*)?$/;
+type LinkData = {
+  videoID?: string;
+  playlistID?: string;
+};
+
+const VIDEO_ID_REGEX = /^[a-zA-Z0-9-_]{11}$/;
+const YOUTUBE_URL_REGEX =
+  /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+
 @Injectable()
 export class SearchService {
-  private isVideoLink(link: string) {
-    const match = link.match(MATCH_VIDEO_LINK);
-    if (match && match[4]?.length > 0) {
-      return true;
+  private isVideoLink(url: string): LinkData | undefined {
+    if (!YOUTUBE_URL_REGEX.test(url)) {
+      return;
     }
-    return false;
-  }
+    const location = new URL(url);
+    const params = new URLSearchParams(location.search);
 
-  /**
-   * Return the playlist ID if the link is a playlist link
-   * @param link link to check
-   * @returns playlist ID if the link is a playlist link, undefined otherwise
-   */
-  private getPlaylistId(link: string) {
-    const match = link.match(
-      /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/playlist\?list=(.*)$/,
-    );
-    if (match && match[4]?.length > 0) {
-      return match[4];
-    }
-    return undefined;
-  }
-
-  public async fetchVideo(link: string): Promise<Search> {
-    const videos: Video[] = [];
-    const channels: Channel[] = [];
-    const playlists: Playlist[] = [];
-    const mixes: Mix[] = [];
-
-    const info = await ytdl.getInfo(link);
-    if (info.videoDetails.isLiveContent) {
+    // If the url is a video link
+    if (VIDEO_ID_REGEX.test(location.pathname.slice(1))) {
       return {
-        videos,
-        channels,
-        playlists,
-        mixes,
+        videoID: location.pathname.slice(1),
+        playlistID: params.get('list') || undefined,
       };
     }
-
-    videos.push({
-      title: info.videoDetails.title,
-      url: info.videoDetails.video_url,
-      id: info.videoDetails.videoId,
-      duration: getFormattedTime(
-        parseInt(info.videoDetails.lengthSeconds) * 1000,
-      ),
-      durationSeconds: parseInt(info.videoDetails.lengthSeconds),
-      bestThumbnail: {
-        url: info.videoDetails.thumbnails[0].url,
-        width: info.videoDetails.thumbnails[0].width,
-        height: info.videoDetails.thumbnails[0].height,
-      },
-      author: {
-        name: info.videoDetails.author.name,
-        url: info.videoDetails.author.channel_url,
-        channelID: info.videoDetails.author.id,
-      },
-    });
+    const videoID = params.get('v');
+    const playlistID = params.get('list');
     return {
-      videos,
-      channels,
-      playlists,
-      mixes,
+      videoID: videoID || undefined,
+      playlistID: playlistID || undefined,
     };
   }
 
-  public async fetchPlaylist(id: string): Promise<Search> {
-    const videos: Video[] = [];
-    const channels: Channel[] = [];
-    const playlists: Playlist[] = [];
-    const mixes: Mix[] = [];
+  public async fetchVideo(link: string): Promise<Video> {
+    const info = await video_info(link);
 
+    return {
+      title: info.video_details.title || throwExpression('No video title'),
+      url: info.video_details.url,
+      id: info.video_details.id || throwExpression('No video id'),
+      duration: getFormattedTime(info.video_details.durationInSec * 1000),
+      durationSeconds: info.video_details.durationInSec,
+      bestThumbnail: {
+        url: info.video_details.thumbnails[0].url,
+        width: info.video_details.thumbnails[0].width,
+        height: info.video_details.thumbnails[0].height,
+      },
+      author: {
+        name: info.video_details.channel?.name || throwExpression('No author'),
+        url: info.video_details.channel?.url || throwExpression('No author'),
+        channelID:
+          info.video_details.channel?.id || throwExpression('No author'),
+      },
+    };
+  }
+
+  public async fetchPlaylist(id: string): Promise<Playlist> {
     const playlist = await ytpl(id, {
       hl: 'fr',
       gl: 'FR',
     });
-    playlist.items.forEach((item) => {
-      videos.push({
-        title: item.title,
-        url: item.url,
-        id: item.id,
-        bestThumbnail: {
-          url: item.bestThumbnail.url,
-          width: item.bestThumbnail.width,
-          height: item.bestThumbnail.height,
-        },
-        author: {
-          name: item.author.name,
-          url: item.author.url,
-          channelID: item.author.channelID,
-        },
-        duration: item.duration,
-      });
-    });
-    playlists.push({
+    return {
       title: playlist.title,
       url: playlist.url,
       playlistID: playlist.id,
@@ -126,23 +89,33 @@ export class SearchService {
         url: playlist.author.url,
         channelID: playlist.author.channelID,
       },
-    });
-    return {
-      videos,
-      channels,
-      playlists,
-      mixes,
     };
   }
 
   public async search(query: string): Promise<Search> {
-    const playlistId = this.getPlaylistId(query);
-    if (playlistId) {
-      return this.fetchPlaylist(playlistId);
-    }
+    const videos: Video[] = [];
+    const channels: Channel[] = [];
+    const playlists: Playlist[] = [];
+    const mixes: Mix[] = [];
 
-    if (this.isVideoLink(query)) {
-      return this.fetchVideo(query);
+    const linkData = this.isVideoLink(query);
+    if (linkData) {
+      if (linkData.videoID) {
+        try {
+          videos.push(await this.fetchVideo(linkData.videoID));
+        } catch {}
+      }
+      if (linkData.playlistID) {
+        try {
+          playlists.push(await this.fetchPlaylist(linkData.playlistID));
+        } catch {}
+      }
+      return {
+        videos,
+        channels,
+        playlists,
+        mixes,
+      };
     }
 
     const result = await ytsr(query, {
@@ -150,10 +123,6 @@ export class SearchService {
       gl: 'FR',
       hl: 'fr',
     });
-    const videos: Video[] = [];
-    const channels: Channel[] = [];
-    const playlists: Playlist[] = [];
-    const mixes: Mix[] = [];
 
     result.items.forEach((item) => {
       switch (item.type) {
